1594df679b1caf82ad848eaebf5cef25630b8cb4
ElhamE <elham.esmat@haw-hamburg.de>
2018-07-17 15:37:51 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/elham/bigdatalernprogramm/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.elham.bigdatalernprogramm;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.elham.bigdatalernprogramm.begriffsklaerung.BegriffsklaerungMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // Add code here to update the UI based on the item selected
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+
+        //open and lock the Drawer on start
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_definition:
+                fragmentClass = BegriffsklaerungMenu.class;
+                break;
+            case R.id.nav_challenges:
+                fragmentClass = BegriffsklaerungMenu.class;
+                break;
+            case R.id.nav_dataquiz:
+                fragmentClass = BegriffsklaerungMenu.class;
+                break;
+            case R.id.nav_chancesrisks:
+                fragmentClass = BegriffsklaerungMenu.class;
+                break;
+            default:
+                fragmentClass = BegriffsklaerungMenu.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawerLayout.closeDrawers();
+        //unlock drawer if necessary
+        if (mDrawerLayout.getDrawerLockMode(GravityCompat.START) == DrawerLayout.LOCK_MODE_LOCKED_OPEN){
+            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/elham/bigdatalernprogramm/begriffsklaerung/BegriffsklaerungMenu.java
@@ -0,0 +1,27 @@
+package com.example.elham.bigdatalernprogramm.begriffsklaerung;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.elham.bigdatalernprogramm.R;
+
+public class BegriffsklaerungMenu extends Fragment {
+
+    private TextView mText;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.begriffsklaerung_menu, container, false);
+        mText = (TextView) rootView.findViewById(R.id.text_view_id);
+        mText.setTextColor(Color.RED);
+        mText.setText("Hallo Welt!");
+        return rootView;
+    }
+}
